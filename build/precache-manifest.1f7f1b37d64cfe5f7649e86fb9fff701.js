d007385e8558fecdd51adf84e6de8c4b1fc5c8d6
Vidit Gupta <vidit135g@gmail.com>
2020-07-11 20:56:19 +0530

    initial

---
+++ build/precache-manifest.1f7f1b37d64cfe5f7649e86fb9fff701.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98e78e2d089877481663969996438ce",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "b647d956158d21958710",
+    "url": "/Portfolio/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "121f5df110ab3c32cb6a",
+    "url": "/Portfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "46b0c57ab34290912e8c",
+    "url": "/Portfolio/static/css/main.a78acd80.chunk.css"
+  },
+  {
+    "revision": "a2dfe5ea0a38ead6efa0",
+    "url": "/Portfolio/static/js/2.0a0da556.chunk.js"
+  },
+  {
+    "revision": "b647d956158d21958710",
+    "url": "/Portfolio/static/js/3.60cf51a1.chunk.js"
+  },
+  {
+    "revision": "121f5df110ab3c32cb6a",
+    "url": "/Portfolio/static/js/4.729f5b70.chunk.js"
+  },
+  {
+    "revision": "46b0c57ab34290912e8c",
+    "url": "/Portfolio/static/js/main.689e3d7d.chunk.js"
+  },
+  {
+    "revision": "c7d654d7d412f4e10132",
+    "url": "/Portfolio/static/js/runtime-main.56e770a6.js"
+  },
+  {
+    "revision": "f8c351bf8263f7beefb5c4c444951389",
+    "url": "/Portfolio/static/media/Agustina.f8c351bf.woff"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/Portfolio/static/media/Montserrat-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "225a865fab8deee44e0d7f48686c8c2b",
+    "url": "/Portfolio/static/media/contact.225a865f.PNG"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/Portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "299fde5271cc2acc4386aa3e9505a933",
+    "url": "/Portfolio/static/media/focus.299fde52.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "68832289e6cbce8b500e1160867ebb20",
+    "url": "/Portfolio/static/media/notes.68832289.png"
+  },
+  {
+    "revision": "88c54782480ce9d6136328e6c501fd6c",
+    "url": "/Portfolio/static/media/play.88c54782.png"
+  },
+  {
+    "revision": "ad4fd2f5c7ab770855782cd98497888d",
+    "url": "/Portfolio/static/media/prof.ad4fd2f5.PNG"
+  },
+  {
+    "revision": "ab427bacc32a3612a41c99bde7790964",
+    "url": "/Portfolio/static/media/quoraLogo.ab427bac.png"
+  },
+  {
+    "revision": "e0d9507b602fa2e6d18daa87053eccb4",
+    "url": "/Portfolio/static/media/rep.e0d9507b.png"
+  },
+  {
+    "revision": "86bf7ba78854b01665e01d901e1dd7d4",
+    "url": "/Portfolio/static/media/school.86bf7ba7.png"
+  },
+  {
+    "revision": "3ae2295d8ef55009d64704e1c9622b06",
+    "url": "/Portfolio/static/media/todo.3ae2295d.png"
+  }
+]);
